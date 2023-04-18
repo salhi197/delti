0e224c802492a065ec00cd5664aa2d11840884a8
Salhi Haider ali <salhiali197@gmail.com>
2023-04-18 18:33:18 +0100

    fix logout

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,21 +1,10 @@
 <?php
 
-
-
 namespace App\Http\Middleware;
-
-
-
 use Closure;
-
 use Illuminate\Support\Facades\Auth;
-
-
-
 class RedirectIfAuthenticated
-
 {
-
     public function handle($request, Closure $next, $guard = null)
     {
         if ($guard === 'admin' && Auth::guard($guard)->check()) {
@@ -33,12 +22,10 @@ class RedirectIfAuthenticated
         if ($guard === 'boutique' && Auth::guard($guard)->check()) {
             return redirect('/home');
         }
-
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
         return $next($request);
-
     }
 
 }
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -99,15 +99,6 @@
                         </div>
                     </div>
 
-                    <div class="dropdown d-inline-block">
-                        <a href="" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="align-middle"><a href="{{route('lang',['lang'=>'fr'])}}" class="text-white"> Ar </a></span>
-                        </a>
-                        <a href="" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="align-middle"><a href="{{route('lang',['lang'=>'fr'])}}" class="text-white"> Fr </a></span>
-                        </a>
-
-                    </div>
 
                     <div class="dropdown d-none d-lg-inline-block ms-1">
                         <button type="button" class="btn header-item noti-icon waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -117,22 +108,12 @@
                             <div class="px-lg-2">
                                 <div class="row g-0">
                                     <div class="col">
-                                        <a class="dropdown-icon-item" href="#">
-                                            <i class=" fas fa-assistive-listening-systems" style="color:#C64756;font-size: 19px;"></i>
-                                            <span style="color:#7d858c;font-size: 15px;">+ {{trans('main.audience')}}</span>
-                                        </a>
-                                    </div>
-                                    <div class="col">
-                                        <a class="dropdown-icon-item" href="#">
-                                            <i class="  fas fa-list-ul" style="color:#C64756;font-size: 19px;"></i>
-                                            <span style="color:#7d858c;font-size: 15px;">+ Requête</span>
-                                        </a>
-                                    </div>
-                                    <div class="col">
-                                        <a class="dropdown-icon-item" href="#">
-                                            <i class=" fas fa-book" style="color:#C64756;font-size: 19px;"></i>
-                                            <span style="color:#7d858c;font-size: 15px;">+ Doléance</span>
-                                        </a>
+                                        <a class="dropdown-item text-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
+                                                class="ri-shut-down-line align-middle me-1 text-danger"></i> Déconnexion</a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+
                                     </div>
                                 </div>
 
@@ -140,95 +121,6 @@
                         </div>
                     </div>
 
-                    <div class="dropdown d-none d-lg-inline-block ms-1">
-                        <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
-                            <i class="ri-fullscreen-line"></i>
-                        </button>
-                    </div>
-
-                    <div class="dropdown d-inline-block">
-                        <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="ri-notification-3-line"></i>
-                            <span class="noti-dot"></span>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0" aria-labelledby="page-header-notifications-dropdown">
-                            <div class="p-3">
-                                <div class="row align-items-center">
-                                    <div class="col">
-                                        <h6 class="m-0"> Notifications </h6>
-                                    </div>
-                                    <div class="col-auto">
-                                        <a href="#!" class="small"> View All</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div data-simplebar style="max-height: 230px;">
-                                <a href="#" class="text-reset notification-item">
-                                    <div class="d-flex">
-                                        <div class="avatar-xs me-3">
-                                            <span class="avatar-title bg-primary rounded-circle font-size-16">
-                                                <i class="ri-shopping-cart-line"></i>
-                                            </span>
-                                        </div>
-                                        <div class="flex-1">
-                                            <h6 class="mb-1">Your order is placed</h6>
-                                            <div class="font-size-12 text-muted">
-                                                <p class="mb-1">If several languages coalesce the grammar</p>
-                                                <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 3 min ago</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="#" class="text-reset notification-item">
-                                    <div class="d-flex">
-                                        <img src="" class="me-3 rounded-circle avatar-xs" alt="user-pic">
-                                        <div class="flex-1">
-                                            <h6 class="mb-1">James Lemire</h6>
-                                            <div class="font-size-12 text-muted">
-                                                <p class="mb-1">It will seem like simplified English.</p>
-                                                <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 1 hours ago</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="#" class="text-reset notification-item">
-                                    <div class="d-flex">
-                                        <div class="avatar-xs me-3">
-                                            <span class="avatar-title bg-success rounded-circle font-size-16">
-                                                <i class="ri-checkbox-circle-line"></i>
-                                            </span>
-                                        </div>
-                                        <div class="flex-1">
-                                            <h6 class="mb-1">Your item is shipped</h6>
-                                            <div class="font-size-12 text-muted">
-                                                <p class="mb-1">If several languages coalesce the grammar</p>
-                                                <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 3 min ago</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-
-                                <a href="#" class="text-reset notification-item">
-                                    <div class="d-flex">
-                                        <div class="flex-1">
-                                            <h6 class="mb-1">Salena Layfield</h6>
-                                            <div class="font-size-12 text-muted">
-                                                <p class="mb-1">As a skeptical Cambridge friend of mine occidental.</p>
-                                                <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 1 hours ago</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="p-2 border-top">
-                                <div class="d-grid">
-                                    <a class="btn btn-sm btn-link font-size-14 text-center" href="javascript:void(0)">
-                                        <i class="mdi mdi-arrow-right-circle me-1"></i> View More..
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
 
                     <div class="dropdown d-inline-block user-dropdown">
