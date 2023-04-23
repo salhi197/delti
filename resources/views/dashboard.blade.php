d469a13e05dbee8bd4e43428909ba35b71006ab0
Haider Ali Salhi <salhiali197@gmail.com>
2023-04-23 19:49:26 +0200

    add copy to clipboard 2.0

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,7 +35,7 @@
                         <div class="col-12">
                             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                                 <div class="input-group">
-                                   <input type="text"  readonly="" 
+                                   <input type="text" id="myInput" readonly="" 
                                    value="deltti.com/c/{{Auth::guard('client')->user()->id}}" class="form-control" >
 
                                     <span class="input-group-btn">
