160ef6a5f43b399ad3e6176df3a9eb9c854b9833
Haider Ali Salhi <salhiali197@gmail.com>
2023-04-25 17:57:40 +0200

    add notification for user

--- resources/views/compteur.blade.php
+++ resources/views/compteur.blade.php
@@ -10,8 +10,9 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h3 class="mb-sm-0">File D'attente :  {{$client->nom ?? $client->prenom ?? ''}}</h3>
-                            </div>
+                                <h3 class="mb-sm-0">Pour assurer le bon fonctionnement , Vous devez vous présentez avant deux (2) tickets de votre tour  </h3>
+                                <h3 class="mb-sm-0">Le ticket Actuel est :</h3>    
+                            </div>                          
                         </div>
                     </div>
 
@@ -19,6 +20,27 @@
                         <div class="col-md-12">
                             <h1 id="compteur" style="font-size:30px;" >{{$count ?? 0}}</h1>
                         </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card-body align-items-center d-flex justify-content-center">
+                                    <div class="d-flex">
+                                        <form method="post" action="#">
+                                            @csrf
+                                            <div class="input-group">
+                                              <label>créer une notification selon votre numéro de ticket  : </label>
+                                                <input type="text" name="count"  class="form-control" >
+                                                  <span class="input-group-btn">
+                                                  <button  type="submit" class="btn btn-success"  ><i class="fa fa-clone"></i></button>
+                                                </span>
+                                            </div>                                                                       
+
+                                        </form>
+
+                                    </div>
+                                </div><!-- end cardbody -->
+
+                        </div><!-- end col -->
                     </div>
 
 @endsection
